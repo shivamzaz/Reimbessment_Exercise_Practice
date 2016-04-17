ab7153cbf9f0556f17f5291a7d49c4e72434dff8
Shivam Gupta <shivam.gupta712@gmail.com>
2016-04-18 03:45:46 +0530

    just update

--- monk_3.c
+++ monk_3.c
@@ -2,14 +2,14 @@
 int main(){
 	//int t;
 	long long int i,a[100000],t;
-	scanf("%lld",&t);
+	//scanf("%lld",&t);
 	for(i=0;i<t;i++){
 	scanf("%lld",&a[i]);}
-	for(i=1;i<t;i++){
+	/*for(i=1;i<t;i++){
 		if(a[i]==0){
 			a[i-1]=0;
 		}
-	}
+	}*/
 	for(i=0;i<t;i++){
 		if(a[i]!=0){
 			printf("%lld\n",a[i]);
