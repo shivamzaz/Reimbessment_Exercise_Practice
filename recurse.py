09f1079791e68108a65d43c7e6a4a3c66d6d8028
Shivam Gupta <shivam.gupta712@gmail.com>
2016-07-04 01:31:08 +0530

    Two nested loops  using recursion in python

--- recurse.py
+++ recurse.py
@@ -6,13 +6,11 @@ def s_f(i,n):
 	if(i<n):	
 		j=0
 		s_g(i,j,n)
-		i+=1
-		s_f(i,n)
+		s_f(i+1,n)
 def s_g(i,j,n):
 	if(j<n):
 		print i,j
-		j+=1
-		s_g(i,j,n)
+		s_g(i,j+1,n)
 s_s(4)
 '''for(i=0;i<n;i++){
 	for(j=0;j<nj++){
